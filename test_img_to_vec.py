7ef777d3c870d092694268dac0500aa8e84bc8b9
Unknown <sushsilsharma.ss451@gmail.com>
2018-04-01 21:58:20 +0530

    modulated csv files

--- test_img_to_vec.py
+++ test_img_to_vec.py
@@ -12,17 +12,18 @@ pics = {}
 
 def read_from_csv():
     # reading csv file
-    with open("images.csv", 'r') as csvfile:
-        # creating a csv reader object
-        csvreader = csv.reader(csvfile)
+    for filename in os.listdir('./csv'):
+        with open('./csv/'+filename, 'r') as csvfile:
+            # creating a csv reader object
+            csvreader = csv.reader(csvfile)
 
-        # extracting each data row one by one
-        for row in csvreader:
-            # Ignore the first `[` and last `]` in row[1]
-            pics[row[0]] = np.fromstring(row[1][1:-1], dtype=np.float_, sep=' ')
+            # extracting each data row one by one
+            for row in csvreader:
+                # Ignore the first `[` and last `]` in row[1]
+                pics[row[1]] = np.fromstring(row[2][1:-1], dtype=np.float_, sep=' ')
 
-        # get total number of rows
-        print("Total no. of rows: %d" % (csvreader.line_num))
+            # get total number of rows
+            # print("Total no. of rows: %d" % (csvreader.line_num))
 
 
 def add_to_dict(pic_name):
@@ -36,11 +37,11 @@ def add_to_dict(pic_name):
 
 
 def main():
-    read_from_csv()
     pic_name = str(input("Enter relative path of the image to search?\n"))
 
     add_to_dict(pic_name)
 
+    read_from_csv()
     try:
         sims = {}
         for key in list(pics.keys()):
@@ -51,7 +52,7 @@ def main():
 
         d_view = [(v, k) for k, v in sims.items()]
         d_view.sort(reverse=True)
-        for v, k in d_view[0:10]:
+        for v, k in d_view[0:5]:
             print(v, k)
 
     except KeyError as e:
